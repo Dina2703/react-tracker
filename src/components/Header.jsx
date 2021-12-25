222d4430b9b5e59e6fd6c615c3624adab6f855ab
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-25 12:14:28 -0500

    Styling ways

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-function Header({title, subtitle}) {
+function Header({ title, subtitle }) {
   return (
     <header>
-      <h1>Task Tracker {title} -- {subtitle}</h1>
+      <h1 style={{ color: "yellow", backgroundColor: "black" }}>
+        Task Tracker {title}
+      </h1>
+      <h2 style={subtitleStyle}>{subtitle}</h2>
     </header>
   );
 }
 
 //default props
 Header.defaultProps = {
-  subtitle: 'subtitle'
-}
+  subtitle: "subtitle",
+};
 
 //PropTypes helps you catch errors before they happen
 Header.propTypes = {
-  title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+};
+
+//CSS in JS
+const subtitleStyle = {
+  color: "red",
+  backgroundColor: "gray",
+};
 
 export default Header;
