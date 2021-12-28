e556cc0bcb559420ca92cea7be15bc368b758cf2
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-27 23:58:28 -0500

    useEffect Hook & Fetch tasks from server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ]);
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
 
+  /*The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM*/
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchtasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  //Fetch Tasks
+
+  const fetchtasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
   //Add Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 100) + 1;
@@ -48,7 +46,10 @@ function App() {
   };
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks
